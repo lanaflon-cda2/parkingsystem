c998ee03af58b2b97951e66c457bcb73d31465c4
Slingshot <slingshot@outlook.fr>
2020-03-17 10:51:47 +0100

    ajout de la classe de test ParkingDataBaseBikeITTest

--- src/test/java/it/com/parkit/parkingsystem/ParkingDataBaseBikeITTest.java
+++ src/test/java/it/com/parkit/parkingsystem/ParkingDataBaseBikeITTest.java
@@ -1,32 +1,32 @@
 package it.com.parkit.parkingsystem;
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.read.ListAppender;
+import com.parkit.parkingsystem.constants.Fare;
+import com.parkit.parkingsystem.dao.ClientDAO;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
+import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
+import it.com.parkit.parkingsystem.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
-import it.com.parkit.parkingsystem.service.DataBasePrepareService;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.platform.commons.logging.LoggerFactory;
+import org.apache.logging.log4j.core.config.Order;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Null;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseBikeITTest {
 
-    private static com.parkit.parkingsystem.integration.config.DataBaseTestConfig dataBaseTestConfig = new com.parkit.parkingsystem.integration.config.DataBaseTestConfig();
+    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static DataBasePrepareService dataBasePrepareService;
@@ -36,7 +36,7 @@ public class ParkingDataBaseBikeITTest {
     private static InputReaderUtil inputReaderUtil;
 
     @BeforeAll
-    private static void setUp() throws Exception{
+    private static void setUp() {
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -48,44 +48,46 @@ public class ParkingDataBaseBikeITTest {
     @BeforeEach
     private void setUpPerTest() throws Exception {
         when(inputReaderUtil.readSelection()).thenReturn(2);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
+        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("bike");
         dataBasePrepareService.clearDataBaseEntries();
+    }
+
+    @AfterAll
+    private static void tearDown(){
 
     }
 
     @Test
-    public void doThat() throws Exception {
-        // get Logback Logger
-        //Logger fooLogger = (Logger) LoggerFactory.getLogger(ParkingService.class);
-        Logger logger = LogManager.getLogger("ParkingService");
+    //  @Disabled
+    public void testParkingACarAndExit() throws InterruptedException, CloneNotSupportedException {
+        ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
+        parkingService.processIncomingVehicle();
+        Thread.sleep(2000);
+        parkingService.processExitingVehicle();
 
-        // create and start a ListAppender
-        ListAppender<ILoggingEvent> listAppender = new ListAppender<>();
-        listAppender.start();
+        ticket = ticketDAO.getTicket("bike");
+        assertEquals(0, ticket.getPrice());
+        assertNotNull(ticket.getInTime());
+        assertNotNull(ticket.getOutTime());
+        assertNotNull(ticket.getParkingSpot());
+        assertEquals("bike", ticket.getVehicleRegNumber());
+        //TODO: check that the fare generated and out time are populated correctly in the database : OK
+    }
 
-        // add the appender to the logger
-        // addAppender is outdated now
-      //  fooLogger.addAppender(listAppender);
-
+    @Test
+    //  @Disabled
+    public void testParkingACar() throws InterruptedException, CloneNotSupportedException {
         Date inTime = new Date();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
-        parkingService.processIncomingVehicle();
-        parkingService.processIncomingVehicle();
 
-        // JUnit assertions
-        List<ILoggingEvent> logsList = listAppender.list;
-        assertEquals("Error fetching parking number from DB. Parking slots might be full", logsList.get(0)
-                .getMessage());
-        assertEquals(Level.ERROR, logsList.get(0)
-                .getLevel());
-
-        assertEquals("finish", logsList.get(1)
-                .getMessage());
-        assertEquals(Level.ERROR, logsList.get(1)
-                .getLevel());
+        ticket = ticketDAO.getTicket("bike");
+        assertEquals(0, ticket.getPrice());
+        assertNotNull(ticket.getInTime());
+        assertNotNull(ticket.getParkingSpot());
+        assertEquals("bike", ticket.getVehicleRegNumber());
+        //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability :OK
     }
+
 }
-
-
 
